1a44f5df8bc7c10268a6cd08ced234c97491028f
Роман Кораблев <korabl79@mail.ru>
2025-01-18 06:47:44 +0300

    create PostForm

---
+++ blogicum/blog/form.py
@@ -0,0 +1,24 @@
+from django import forms
+# from django.contrib.auth import get_user_model
+
+from .models import Post  # , Comment
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'pub_date', 'image', 'category', 'location']
+        widgets = {
+            'pub_date': forms.DateTimeInput(
+                format='%Y-%m-%dT%H:%M',
+                attrs={'type': 'datetime-local'}
+            )
+        }
+
+
+# class CommentForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Comment
+#         fields = ['text']
